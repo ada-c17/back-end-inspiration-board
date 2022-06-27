e0466ab1380ff5cba70664d1afb4985fab379ec1
Lauren Kleinert <lauren.o.kleinert@gmail.com>
2022-06-27 15:57:01 -0700

    Created Board and Card modelsand migration folder

--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,8 @@
 from app import db
+
+class Card (db.Model):
+    card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    message= db.Column(db.String, nullable=False)
+    likes_count = db.Column(db.Integer, nullable=False)
+    board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'), nullable=True)
+    board = db.relationship("Board", back_populates="cards")
