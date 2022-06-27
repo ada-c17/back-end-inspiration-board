9f159771461a30f2b34833c46dd705826b1cef3b
Vida Maleki <vida.basmani@gmail.com>
2022-06-27 15:55:29 -0700

    added card and board class to modeles, created card and board routes

--- app/__init__.py
+++ app/__init__.py
@@ -20,12 +20,20 @@ def create_app():
     # Import models here for Alembic setup
     # from app.models.ExampleModel import ExampleModel
 
+    from app.models.board import Board
+    from app.models.card import Card
+
     db.init_app(app)
     migrate.init_app(app, db)
 
     # Register Blueprints here
     # from .routes import example_bp
     # app.register_blueprint(example_bp)
+
+    from .board_routes import boards_bp
+    from .card_routes import cards_bp
+    app.register_blueprint(boards_bp)
+    app.register_blueprint(cards_bp)
 
     CORS(app)
     return app
---
+++ app/card_routes.py
@@ -0,0 +1,6 @@
+from flask import Blueprint, request, jsonify, make_response
+from app import db
+from app.models.card import Card
+
+# example_bp = Blueprint('example_bp', __name__)
+cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
