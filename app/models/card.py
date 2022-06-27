9b470e89e333801bb3c4309bc97e57c1a52f6abb
Lindsey Still <lindseymstill@gmail.com>
2022-06-28 04:55:18 +0900

    added card and board models

--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,8 @@
 from app import db
+
+class Card(db.Model):
+	card_id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+	message = db.Column("message",db.String, nullable = False)
+	likes_count = db.Column("likes_count", db.Integer, default = 0)
+	board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'))
+	board = db.relationship("Board", back_populates = "cards")
