7f6191d7a2ff98c188fe71aeb6e81d437f77d0a8
Andrea Garcia <andrygzt@gmail.com>
2022-06-27 21:02:47 -0700

    DB and migration sucessfully

--- app/models/board.py
+++ app/models/board.py
@@ -1,6 +1,6 @@
 from app import db
 
-class Boad(db.Model):
-    board_id = db.Column(db.Integer, primar_key=True, autoincrement=True)
+class Board(db.Model):
+    board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
