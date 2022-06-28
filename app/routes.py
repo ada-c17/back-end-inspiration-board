d1af002b74c7cfb4cd9b60a2f4d79f4733aa8afe
Lin Liu <lin_6066@yahoo.com>
2022-06-28 11:46:13 -0700

    add route for update like count

--- app/routes.py
+++ app/routes.py
@@ -7,9 +7,11 @@ from app.models.board import Board
 import requests
 
 # example_bp = Blueprint('example_bp', __name__)
-board_bp = Blueprint("board_bp", __name__, url_prefix = "/boards")
+board_bp = Blueprint("board_bp", __name__, url_prefix="/boards")
+card_bp = Blueprint("card_bp", __name__, url_prefix="/cards")
 
-@board_bp.route("", methods = ["GET"])
+
+@board_bp.route("", methods=["GET"])
 def get_all_boards():
     boards = Board.query.all()
     board_response = []
@@ -21,18 +23,21 @@ def get_all_boards():
         })
     return jsonify(board_response), 200
 
+
 @board_bp.route("", methods=["POST"])
 def create_one_board():
     request_body = request.get_json()
     try:
-        new_board = Board(title = request_body["title"], owner = request_body["owner"])
+        new_board = Board(
+            title=request_body["title"], owner=request_body["owner"])
     except KeyError:
-        return {"msg":"Invalid input"}, 400
+        return {"msg": "Invalid input"}, 400
         # abort(make_response(jsonify(response), 400))
     db.session.add(new_board)
     db.session.commit()
-    response = {"board":{"title":new_board.title, "owner": new_board.owner}}
+    response = {"board": {"title": new_board.title, "owner": new_board.owner}}
     return jsonify(response), 201
+
 
 @board_bp.route("/<board_id>/cards", methods=["POST"])
 def create_one_card(board_id):
@@ -41,10 +46,21 @@ def create_one_card(board_id):
         new_message = request_body["message"]
         if len(new_message) > 40 or not new_message:
             abort(make_response(jsonify({"msg": "invalid card message"}), 400))
-        new_card = Card(message=request_body["message"], likes_count=0, board_id=board_id)
+        new_card = Card(
+            message=request_body["message"], likes_count=0, board_id=board_id)
     except KeyError:
-        return {"msg":"Invalid input"}, 400
+        return {"msg": "Invalid input"}, 400
     db.session.add(new_card)
     db.session.commit()
-    response = {"card":{"message":new_card.message, "id": new_card.card_id}}
+    response = {"card": {"message": new_card.message, "id": new_card.card_id}}
     return jsonify(response), 201
+
+
+@card_bp.route("/<card_id>", methods=["PATCH"])
+def update_card_likecount(card_id):
+    card = Card.query.get(card_id)
+    card.likes_count += 1
+    db.session.commit()
+
+    response = {"msg": f"update like count to {card.likes_count}"}
+    return jsonify(response), 200
