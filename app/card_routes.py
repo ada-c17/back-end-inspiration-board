27b114c1c0f690b713f549b95d76f9e7b420fc6b
Danielle <dwhyte1226@gmail.com>
2022-06-28 12:46:34 -0500

    added create and patch methods for Card model --NOT TESTED

--- app/card_routes.py
+++ app/card_routes.py
@@ -1,5 +1,29 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from models import Card
 
 # example_bp = Blueprint('example_bp', __name__)
-card_bp = Blueprint('card_bp', __name__)
+card_bp = Blueprint('card_bp', __name__, url_prefix="/board/")
+
+@card_bp.route("<board_id>/card", methods=["CREATE"])
+def create_card(board_id):
+    #validate
+    request_body = request.get_json()
+    new_card = Card(message=request_body["message"], board_id=board_id)
+
+    db.session.add(new_card)
+    db.session.commit()
+
+    return make_response("Success", 201)
+
+@card_bp.route("<board_id>/card/<card_id>", methods=["PATCH"])
+def update_card(board_id, card_id):
+    #validate board_id
+    #validate card_id
+    card = Card.query.get(card_id)
+
+    request_body = request.get_json()
+    card["message"] = request_body["message"]
+
+    db.session.commit()
+    return make_response("Success", 200)
