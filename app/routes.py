3d5b53da3c8400b3dd9cd3fbf18114aaffaf1f4a
Ada <adabarries@gmail.com>
2022-06-28 12:02:00 -0700

    adds create_one_card() and get_all_cards_by_board_id()

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
+from crypt import methods
 from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
 from .models.board import Board
 from .models.card import Card
-from app.models import board
 
 boards_bp = Blueprint("boards_bp", __name__, url_prefix="/boards")
 cards_bp = Blueprint("card_bp", __name__, url_prefix="/cards")
@@ -62,10 +62,42 @@ def get_all_boards():
 # @boards_bp.route("/<board_id>", methods=[GET])    
 
 # ***** POST /boards/<board_id>/cards *****
+@boards_bp.route('/<board_id>/cards', methods=["POST"])
+def create_one_card(board_id):
+    request_body = request.get_json()
+    
+    if not "message" in request_body:
+        return jsonify({
+            "details": "Must include message"
+        }), 400
+    
+    new_card = Card(message=request_body["message"], board_id=board_id)
 
+    db.session.add(new_card)
+    db.session.commit()
+
+    return {
+        "card_id": new_card.card_id, 
+        "board_id": new_card.board_id,
+        "message": new_card.message
+    }
 
 # ***** GET /boards/<board_id>/cards *****
+@boards_bp.route("/<board_id>/cards", methods=["GET"])
+def get_all_cards_by_board_id(board_id):
+    selected_board = validate_board(board_id)
 
+    cards_response = []
+
+    for card in selected_board.cards: 
+        cards_response.append({
+            "card_id": card.card_id,
+            "message": card.message,
+            "likes_count": 0, # change this to card.likes_count later
+            "board_id": card.board_id
+        })
+    
+    return jsonify(cards_response), 200
 
 # ***** DELETE /cards/<card_id> *****
 
