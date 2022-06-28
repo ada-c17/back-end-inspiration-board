5d24d2ac64d9ad96d8a1d39c9f5d16ff9ff4ff6f
Vida Maleki <vida.basmani@gmail.com>
2022-06-28 10:13:37 -0700

    added get function to board, added card and board relationship

--- app/board_routes.py
+++ app/board_routes.py
@@ -4,3 +4,10 @@ from app.models.board import Board
 
 # example_bp = Blueprint('example_bp', __name__)
 boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
+
+
+#get all elements 
+@boards_bp.route("", methods = ["GET"])
+def get_all_boards():
+    boards = Board.query.all()
+    return jsonify([board.to_dict_board() for board in boards]), 200
