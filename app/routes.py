8b8211715cbc9a211a5d47a8c5b130ab38c61f53
Julie <jawarren89@gmail.com>
2022-06-28 15:57:53 -0700

    JW: adds validate or abort helper function to check board/card ids

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,20 @@ from app import db
 from app.models.board import Board 
 
 
+def validate_or_abort(board_id):
+    # returns 400 error if invalid board_id (alpha/non-int) 
+    try:
+        board_id = int(board_id)
+    except ValueError:
+        abort(make_response({"error": f"{board_id} is an invalid board id"}, 400))
+    
+    # returns 404 error if board_id not found in database
+    board = Board.query.get(board_id)
+    if not board:
+        abort(make_response({"error": f"Board {board_id} not found"}, 404))
+    return board
+
+
 boards_bp = Blueprint('boards_bp', __name__, url_prefix='/boards')
 
 @boards_bp.route('', methods=['GET'])
@@ -13,9 +27,9 @@ def list_all_boards():
     for board in boards:
         board_response.append(
             {
-                "id":board.board_id,
-                "title":board.title,
-                "owner":board.owner,
+                "id": board.board_id,
+                "title": board.title,
+                "owner": board.owner,
             }
         )
     return jsonify(board_response)
@@ -35,15 +49,15 @@ def create_board():
     db.session.commit()
     
     return {
-        "owner": new_board.owner,
         "id": new_board.board_id,
-        "title": new_board.title
+        "owner": new_board.owner,
+        "title": new_board.title,
     }, 201
 
 @boards_bp.route("/<board_id>", methods=["DELETE"])
 def delete_board(board_id):
-    board = Board.query.get(board_id)
-    
+    board = validate_or_abort(board_id)
+
     db.session.delete(board)
     db.session.commit()
 
