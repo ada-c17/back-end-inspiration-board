7d22ac0f0fc1a8f4a38e8093478f2cfa44001628
Sreymompech <sanchhen2013@gmail.com>
2022-06-28 10:49:53 -0700

    SP: Adding post function on board

--- app/board_routes.py
+++ app/board_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
 from app.models.board import Board
+from helper_function import validate_input_key_for_post_or_update
 
 # example_bp = Blueprint('example_bp', __name__)
 boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
@@ -8,4 +9,14 @@ boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
 
 @boards_bp.route("", methods=["POST"])
 def create_board():
+    request_board = validate_input_key_for_post_or_update()
+    new_board = Board(
+        title = request_board["title"],
+        owner = request_board["owner"]
+    )
+
+    db.session.add(new_board)
+    db.session.commit()
+    return jsonify({"board": new_board.to_dict_board()}), 201
+
 
--- app/helper_function.py
+++ app/helper_function.py
@@ -1,4 +1,5 @@
 from flask import request, make_response, abort
+from app.models.board import Board
 # helper function for validate key of input
 def validate_input_key_for_post_or_update():
     request_board = request.get_json()
