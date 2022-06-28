d2ca772507261e257287d18020dc748056aec179
Raha <raha.mora1@gatech.edu>
2022-06-28 16:31:54 -0400

    RahaR_Post-Get-Get-by-id-Delete-by-id-routes-added

--- app/models/board.py
+++ app/models/board.py
@@ -5,7 +5,30 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
-    
+    cards = db.relationship("Card", backref="board", lazy=True)
+
+
+    def make_json(self):
+        board_dic = {
+        "id": self.board_id,
+        "title": self.title,
+        "owner": self.owner,
+    }    
+        return board_dic
+
+
+    @classmethod
+    def valid_board(cls, request_body):
+        try:
+            try:
+                new_board = cls(title = request_body["title"], description = request_body["owner"])
+            except KeyError:
+                new_board = cls(
+                title=request_body['title'],
+                owner=request_body['owner'])
+            return new_board
+        except KeyError:
+            abort(make_response({"details": "Invalid data"}, 400)) 
 
 
 
---
+++ app/routes/board_routes.py
@@ -0,0 +1,82 @@
+from flask import Blueprint, request, jsonify, make_response, abort
+from app.models.board import Board
+from app import db
+from sqlalchemy import asc, desc
+
+
+boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
+
+def validate_key():
+    request_board = request.get_json()
+    if "title" not in request_board or "owner" not in request_board:
+        abort(make_response({"details": "Invalid data"}, 400))
+    return request_board
+
+# helper function for validate id
+def get_board_or_abort(board_id):
+    try:
+        board_id = int(board_id)
+    except ValueError:
+        abort(make_response({"message": f"The task id {board_id} is invalid. The id must be integer."}, 400))
+    
+    boards = Board.query.all()
+    for board in boards:
+        if board.board_id == board_id:
+            return board
+    abort(make_response({"message": f"The task id {board_id} is not found"}, 404))
+
+@boards_bp.route("", methods=["POST"])
+def create_board():
+    request_body = request.get_json()
+    if "title" not in request_body or "owner" not in request_body:
+        return jsonify({"details": "Invalid data"}), 400
+
+    new_board = Board(
+        title = request_body["title"],
+        owner = request_body["owner"]
+    )
+
+    db.session.add(new_board)
+    db.session.commit()
+    return jsonify({"board": new_board.make_json()}), 201
+
+# get all elements 
+@boards_bp.route("", methods = ["GET"])
+def get_all_boards():
+    sort_query = request.args.get("sort")
+    if sort_query == "asc":
+        boards = Board.query.order_by(asc(Board.title))
+    elif sort_query == "desc":
+        boards = Board.query.order_by(desc(Board.title))
+    else:
+        boards = Board.query.all()
+    return jsonify([board.make_json() for board in boards]), 200
+
+
+#get one board using Get in routes and accessing by id
+@boards_bp.route("/<board_id>", methods= ["GET"])
+def get_one_board(board_id):
+    board = validate_board(board_id)
+    return jsonify({"board":board.make_json()}), 200
+
+# validating board and using as a helper function 
+def validate_board(board_id):
+    try:
+        board_id = int(board_id)
+    except ValueError:
+        abort(make_response({"message": f"The task id {board_id} is invalid. The id must be integer."}, 400))
+    
+    boards = Board.query.all()
+    for board in boards:
+        if board.board_id == board_id:
+            return board
+    abort(make_response({"message": f"The task id {board_id} is not found"}, 404))
+
+@boards_bp.route("/<board_id>", methods=["DELETE"])
+def delete_a_board(board_id):
+    board = validate_board(board_id)
+
+    db.session.delete(board)
+    db.session.commit()
+
+    return make_response({'details':f'Task {board.board_id} "{board.title}" successfully deleted'},200)
