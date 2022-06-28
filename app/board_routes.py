aad9e243eb4882e547a9971ec1a07a884fd6410d
Poppy Meng <qim17@pitt.edu>
2022-06-28 10:28:48 -0700

    finish board part

--- app/board_routes.py
+++ app/board_routes.py
@@ -13,6 +13,31 @@ def get_all_boards():
     return jsonify(board_response), 200
 
 
+def validate_board(cls, id):
+    try:
+        board_id = int(id)
+    except:
+        abort(make_response({"details": "Invalid data"}, 400))
+    obj = cls.query.get(id)
+    if not obj:
+        abort(make_response({"message": f"{cls.__name__} not found"}, 404))
+        
+    return obj
+@boards_bp.route("/<board_id>/cards", methods=["GET"])
+def get_cards_by_board(board_id):
+    board_id = int(board_id)
+    board = validate_board(board_id)
+    card_list = []
+    cards = Card.query.filter_by(board_id=board_id)
+    for card in cards:
+        card_list.append(card.to_json())
+
+    return {"id": board_id,
+            "title": board.title,
+            "cards": card_list, 
+            'owner': board.owner}, 200
+
+
 @boards_bp.route("", methods=["POST"])
 def create_board():
     request_body = request.get_json()
