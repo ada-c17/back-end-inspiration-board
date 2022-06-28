1704e4388581a97c4f417f891305755c11b1b4e1
Vida Maleki <vida.basmani@gmail.com>
2022-06-28 11:45:17 -0700

    merging Nish changes

--- app/board_routes.py
+++ app/board_routes.py
@@ -12,12 +12,12 @@ boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
 @boards_bp.route("", methods=["POST"])
 def create_board():
     request_body = request.get_json()
-    if "title" not in request_board or "owner" not in request_board:
+    if "title" not in request_body or "owner" not in request_body:
         return jsonify({"details": "Invalid data"}), 400
 
     new_board = Board(
-        title = request_board["title"],
-        owner = request_board["owner"]
+        title = request_body["title"],
+        owner = request_body["owner"]
     )
 
     db.session.add(new_board)
@@ -29,9 +29,9 @@ def create_board():
 def get_all_boards():
     sort_query = request.args.get("sort")
     if sort_query == "asc":
-        boards = Board.query.order_by(asc(Task.title))
+        boards = Board.query.order_by(asc(Board.title))
     elif sort_query == "desc":
-        boards = Board.query.order_by(desc(Task.title))
+        boards = Board.query.order_by(desc(Board.title))
     else:
         boards = Board.query.all()
     return jsonify([board.to_dict_board() for board in boards]), 200
@@ -40,17 +40,18 @@ def get_all_boards():
 #get one board using Get in routes and accessing by id
 @boards_bp.route("/<board_id>", methods= ["GET"])
 def get_one_board(board_id):
+    board = Board.query.get(id)
     board = get_board_or_abort(board_id)
     if board.card_id:
         response = {"board":{
             "id": board.board_id,
-            "card_id": board.card_id,
+            "cards": board.card_id,
             "title": board.title,
             "owner": board.owner
         }
     }
     else:
-        response = {"board":board.to_dict_board()}
+    response = {"board":board.to_dict_board()}
     return jsonify(response), 200
 
 
@@ -62,7 +63,7 @@ def get_board_or_abort(board_id):
     
     boards = Board.query.all()
     for board in boards:
-        if board.id == board_id:
+        if board.board_id == board_id:
             return board
     abort(make_response({"message": f"The task id {board_id} is not found"}, 404))
 
