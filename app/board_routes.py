f4dee62333f0c1f216f8f03adbdf5a3727afaad0
Sreymompech <sanchhen2013@gmail.com>
2022-06-28 10:17:00 -0700

    Adding herlper function for baord route

--- app/board_routes.py
+++ app/board_routes.py
@@ -4,3 +4,12 @@ from app.models.board import Board
 
 # example_bp = Blueprint('example_bp', __name__)
 boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
+
+# helper function for validate key of input
+def validate_input_key_for_post_or_update():
+    request_board = request.get_json()
+    if "title" not in request_board or "owner" n
+    ot in request_board:
+        abort(make_response({"details": "Invalid data"}, 400))
+@boards_bp.route("", methods=["POST"])
+def create_board():
