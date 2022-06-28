3b7778dff328a7c5f697fdb101f7507510776112
Hope Olaide <hopeolaide@gmail.com>
2022-06-27 20:12:05 -0400

    Models updated...again.

--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,16 @@
 from app import db
+# from board import Board
+class Card(db.Model):
+    card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    message = db.Column(db.String, nullable=False)
+    likes_count = db.Column(db.Integer, autoincrement=True)
+    board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'))
+
+    board = db.Relationship('Board',back_populates='cards')
+
+    def to_dict(self):
+            return {
+                "card_id": self.card_id,
+                "message": self.message,
+                "likes_count": self.likes_count
+            }
