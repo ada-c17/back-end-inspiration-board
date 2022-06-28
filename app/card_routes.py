a6d8922170cf1907daba94a6e5d9383669e61a60
Jodi Denney <iarrived@gmail.com>
2022-06-28 11:17:17 -0700

    card id validator helper function

--- app/card_routes.py
+++ app/card_routes.py
@@ -1,7 +1,23 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, abort, request, jsonify, make_response
 from app import db
+from app.models.card import Card
 
 card_bp = Blueprint("card_bp", __name__, url_prefix="/cards")
+
+
+def validate_card(card_id):
+    try:
+        card_id = int(card_id)
+    except:
+        abort(make_response({"message": f"card {card_id} invalid"}, 400))
+
+    card = Card.query.get(card_id)
+
+    if not card:
+        abort(make_response({"message": f"card {card_id} not found"}, 404))
+
+    return card
+
 
 # GET /boards/<board_id>/cards
 
