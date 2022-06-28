aaf9e39682d590e44ef57a2c18e024447e82fa8e
Amber <ambertanaka@yahoo.com>
2022-06-28 11:03:59 -0700

    at: added POST route for boards and tested it

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
 from app.models.board import Board 
 
@@ -19,3 +19,20 @@ def list_all_boards():
             }
         )
     return jsonify(board_response)
+
+@boards_bp.route('', methods=['POST'])
+def create_board():
+    request_body = request.get_json()
+    try:
+        new_board = Board(title=request_body['title'], owner=request_body['owner'])
+    except:
+        abort(make_response({'details':f'Invalid data'}, 400))
+        
+    db.session.add(new_board)
+    db.session.commit()
+    
+    return {
+        "owner": new_board.owner,
+        "id": new_board.board_id,
+        "title": new_board.title
+    }, 201
