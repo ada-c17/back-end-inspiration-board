e3873cc80ecdde28045a682e65a9fa69ab9f47ac
Danielle <dwhyte1226@gmail.com>
2022-06-28 10:58:45 -0500

    deleted models and migrations folder

--- app/models/board.py
+++ app/models/board.py
@@ -1,6 +1,1 @@
 from app import db
-
-class Board(db.Model):
-    board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String, nullable=False)
-    owner = db.Column(db.String, nullable=False)
