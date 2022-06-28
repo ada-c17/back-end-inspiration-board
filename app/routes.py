c09e91f284ff76820267c0ec689e0773d9e26790
Amber <ambertanaka@yahoo.com>
2022-06-28 10:38:35 -0700

    at: added GET endpoint for boards and tested it

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,21 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.board import Board 
 
-# example_bp = Blueprint('example_bp', __name__)
+
+boards_bp = Blueprint('boards_bp', __name__, url_prefix='/boards')
+
+@boards_bp.route('', methods=['GET'])
+def list_all_boards():
+    board_query = request.args.get("sort")
+    boards = Board.query.all()
+    board_response = []
+    for board in boards:
+        board_response.append(
+            {
+                "id":board.board_id,
+                "title":board.title,
+                "owner":board.owner,
+            }
+        )
+    return jsonify(board_response)
