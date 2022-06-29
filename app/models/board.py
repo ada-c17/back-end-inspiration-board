ec867570b94ef0fcef485e4853b4f85ce731cea9
Saria Pastor <saria.pastor@protonmail.com>
2022-06-29 18:46:49 -0400

    Adds as_dict() method to Board and Card models

--- app/models/board.py
+++ app/models/board.py
@@ -3,5 +3,13 @@ from app import db
 class Board(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
-    owner = db.Column (db.String)
-    cards = db.relationship("Card",back_populates="board", lazy = True)
+    owner = db.Column(db.String)
+    cards = db.relationship("Card", back_populates="board", lazy = True)
+
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'owner': self.owner,
+            'cards': [card.as_dict() for card in self.cards]
+        }
--- app/models/card.py
+++ app/models/card.py
@@ -6,3 +6,10 @@ class Card(db.Model):
     likes_count = db.Column(db.Integer)
     board_id = db.Column(db.Integer, db.ForeignKey('board.id'))
     board = db.relationship("Board", back_populates="cards")
+    
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'message': self.message,
+            'likes_count': self.likes_count
+        }
