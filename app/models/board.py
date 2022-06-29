52815763ab65463556c45a58a0d819fff250a55b
mit <mit.magh@gmail.com>
2022-06-29 11:10:20 -0700

    mm:updated model and added nullable to board_id

--- app/models/board.py
+++ app/models/board.py
@@ -5,4 +5,4 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
-    cards = db.relationship("Card", back_populates="board")
+    cards = db.relationship("Card", back_populates="board", lazy= True)
--- app/models/card.py
+++ app/models/card.py
@@ -5,7 +5,7 @@ class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
     like_count = db.Column(db.Integer)
-    board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'))
+    board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'), nullable=True)
     board = db.relationship("Board", back_populates="cards")
 
     def get_dict(self):
