a9ac778732f62eaf293bbcbcdac5797e4621c77e
tinghj17 <tinghj17@gmail.com>
2022-06-29 10:52:51 -0700

    Add routes for getting all boards and only one board

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,24 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, abort
 from app.models.board import Board
 from app import db
 
 # example_bp = Blueprint('example_bp', __name__)
 boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
+
+
+# helper function to validate board
+def validate_board(board_id):
+    try:
+        board_id = int(board_id)
+    except:
+        abort(make_response({"message":f"Board ID {board_id} is invalid"}, 400))
+
+    board = Board.query.get(board_id)
+
+    if not board:
+        abort(make_response({"message":f"Board ID {board_id} not found"}, 404))
+
+    return board
 
 # routes
 @boards_bp.route("", methods = ["POST"])
@@ -18,3 +33,21 @@ def create_board():
     db.session.commit()
 
     return make_response({"board":new_board.to_json()}, 201)
+
+@boards_bp.route("", methods = ["GET"])
+def get_all_boards():
+    boards = Board.query.all()
+
+    boards_response = []
+    for board in boards:
+        boards_response.append(board.to_json())
+    
+    return jsonify(boards_response)
+
+@boards_bp.route("/<board_id>", methods = ["GET"])
+def get_one_board(board_id):
+    board = validate_board(board_id)
+
+    return make_response({"board":board.to_json()}, 200)
+
+    
