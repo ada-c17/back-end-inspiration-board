7c392485c220f330f8ec1058697c9b6cd9fa076f
Danielle <dwhyte1226@gmail.com>
2022-06-29 12:17:00 -0500

    created route to read one board

--- app/board_routes.py
+++ app/board_routes.py
@@ -1,5 +1,16 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.board import Board
 
 # example_bp = Blueprint('example_bp', __name__)
-board_bp = Blueprint('board_bp', __name__)
+board_bp = Blueprint('board_bp', __name__, url_prefix="/boards")
+
+@board_bp.route("/<board_id>", strict_slashes=False)
+def get_one_board(board_id):
+    #validate board_id
+    board = Board.query.get(board_id)
+    return {
+            "title": board.title,
+            "owner": board.owner,
+            "cards": [card.to_dict() for card in board.cards]
+            }
