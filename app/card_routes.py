86236e0fb899bbc968073a64c94b3039e8f8a4ec
Sreymompech <sanchhen2013@gmail.com>
2022-06-28 22:28:29 -0700

    SP: adding function for get cards by board and create card by board

--- app/card_routes.py
+++ app/card_routes.py
@@ -4,3 +4,6 @@ from app.models.card import Card
 
 # example_bp = Blueprint('example_bp', __name__)
 cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
+
+# @cards_bp.route("/<card_id>", methods=["DELETE"])
+# def delete_card_by_id(card_id):
--- app/models/card.py
+++ app/models/card.py
@@ -1,3 +1,4 @@
+from urllib import response
 from app import db
 
 
@@ -8,3 +9,12 @@ class Card(db.Model):
     board_id = db.Column(db.Integer, db.ForeignKey("board.board_id"), nullable=True)
     # I checked how to use laze in this website...
     #https://medium.com/@ns2586/sqlalchemys-relationship-and-lazy-parameter-4a553257d9ef
+
+    def card_response_dict(self):
+        response = {
+            "card_id": self.card_id,
+            "message": self.message,
+            "likes_count": self.likes_count,
+            "board_id": self.board_id
+        }
+        return response
