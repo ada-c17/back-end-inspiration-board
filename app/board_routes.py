67527986bb0e0c091d864266b3c837734011b3cd
angelica <djyl.ahn@gmail.com>
2022-06-29 10:34:01 -0700

    moves board routes into board_routes, adds create_board route, adds from_json class method to Board

--- app/board_routes.py
+++ app/board_routes.py
@@ -1,5 +1,33 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
+from app.models.board import Board
+
 
 # example_bp = Blueprint('example_bp', __name__)
-board_bp = Blueprint('board_bp', __name__)
+board_bp = Blueprint('board_bp', __name__, url_prefix="/boards/")
+
+# board routes
+
+# create board
+@board_bp.route("", methods=["POST"])
+def create_board():
+    request_body = request.get_json()
+    try:
+        new_board = Board.from_json(request_body)
+
+        db.session.add(new_board)
+        db.session.commit()
+
+    except:
+        abort(make_response("Invalid data", 400))
+
+    return make_response("Success", 201)
+
+
+# get all boards
+@board_bp.route("", methods=["GET"], strict_slashes=False)
+def get_boards():
+    # do sort stuff
+    boards = Board.query.all()
+    boards_response = [board.to_json() for board in boards]
+    return jsonify(boards_response)
