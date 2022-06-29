2594d0d8c0ec38e3fcecd3dfd48bc50dd58948cc
Joanna <joannaparis7@gmail.com>
2022-06-29 11:30:57 -0700

    added more routes in board

--- app/routes/boards.py
+++ app/routes/boards.py
@@ -1,9 +1,23 @@
 from crypt import methods
-from flask import Blueprint, jsonify, request
+from tempfile import TemporaryFile
+from flask import Blueprint, jsonify, request, abort, make_response
 from app.models.board import Board
 from app import db
 
 boards_bp = Blueprint('boards_bp', __name__, url_prefix='/boards')
+
+def validate_board_id(board_id):
+    try:
+        board_id = int(board_id)
+    except TypeError:
+        abort(make_response({"details": "Board ID is invalid data type"}, 400
+                            ))
+    board = Board.query.get(board_id)
+    if not board:
+        abort(make_response({"details": f"No board with id: {board_id}"}, 404
+                            ))
+    return board
+        
 
 @boards_bp.route('', methods=['GET'])
 def get_all_boards():
@@ -12,6 +26,17 @@ def get_all_boards():
     response_body = [board.to_json() for board in boards]
 
     return jsonify(response_body), 200
+
+
+@boards_bp.route('/<board_id>/cards', methods=['GET'])
+def get_all_cards_from_board(board_id):
+    board = validate_board_id(board_id)
+    cards = board.cards
+    list_all_cards = []
+    for card in cards:
+        list_all_cards.append({'id': card.card_id, 'message': card.message, 'likes_count': card.likes_count})
+    
+    return jsonify(list_all_cards), 200
 
 
 @boards_bp.route('', methods=['POST'])
