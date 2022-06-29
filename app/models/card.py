80ef8a6d27b13dd5f070abca8f0cea262ec2fb7d
Morgan Buntin <mmbuntin@gmail.com>
2022-06-29 13:18:09 -0400

    added methods to model classes

--- app/models/card.py
+++ app/models/card.py
@@ -4,3 +4,18 @@ class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer)
+
+    @classmethod
+    def create(cls, req_body):
+        new_card = cls(
+            message=req_body["message"],
+            likes_count = 0
+        )
+        return new_card
+
+    def to_json(self):
+        return {
+            "card_id": self.card_id,
+            "message": self.message,
+            "likes_count": self.likes_count
+        }
