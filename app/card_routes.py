659251c5568875e9e928e271384e7fe5426ef5a8
angelica <djyl.ahn@gmail.com>
2022-06-29 09:18:53 -0700

    fixes card import statement  in card_routes

--- app/card_routes.py
+++ app/card_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
-from models import Card
+from app.models.card import Card
 
 # example_bp = Blueprint('example_bp', __name__)
 card_bp = Blueprint('card_bp', __name__, url_prefix="/boards/")
