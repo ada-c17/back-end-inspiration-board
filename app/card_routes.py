f4a3010271fde93aefd06a0c9e215d376cc14d81
Nishat <nsalsabil25@gmail.com>
2022-06-28 20:25:34 -0700

    create 2 post routes for cards and 1 get route and updated card model

--- app/card_routes.py
+++ app/card_routes.py
@@ -1,6 +1,23 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
 from app.models.card import Card
+from app.models.board import Board 
 
-# example_bp = Blueprint('example_bp', __name__)
 cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
+
+
+# Create a card
+@cards_bp.route("", methods = ["POST"])
+def create_card(): 
+    request_body = request.get_json()
+    if "message" not in request_body:
+        return {"details": "Invalid data"}, 400
+    
+    new_card = Card(message=request_body["message"])
+    
+            
+    db.session.add(new_card)
+    db.session.commit()
+    return jsonify({
+        "card": new_card.to_dict()
+        }), 201
--- app/models/card.py
+++ app/models/card.py
@@ -2,9 +2,20 @@ from app import db
 
 
 class Card(db.Model):
-    card_id = db.Column(db.Integer, primary_key=True)
+    card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer)
-    board_id = db.Column(db.Integer, db.ForeignKey("board.board_id"), nullable=True)
-    # I checked how to use laze in this website...
-    #https://medium.com/@ns2586/sqlalchemys-relationship-and-lazy-parameter-4a553257d9ef
+    board_id = db.Column(db.Integer, db.ForeignKey("board.board_id"))
+
+
+    def to_dict(self):
+        card_dict = {
+                "id": self.card_id,
+                "message": self.message,
+                "likes_count": self.likes_count
+                }
+                
+        if self.board_id:
+            card_dict["board_id"] = self.board_id
+
+        return card_dict
