bd8ceacab2a81537c3fe5c00acb58763ac433eab
olive <olivealavine@gmail.com>
2022-06-30 11:58:28 -0500

    added comments to each endpoint

--- app/board_routes.py
+++ app/board_routes.py
@@ -6,17 +6,12 @@ from app.models.board import Board
 # example_bp = Blueprint('example_bp', __name__)
 board_bp = Blueprint('board_bp', __name__, url_prefix="/boards/")
 
-@board_bp.route("<board_id>", strict_slashes=False)
+#Get one board and associated cards
+@board_bp.route("<board_id>", methods=["GET"], strict_slashes=False)
 def get_one_board(board_id):
     #validate board_id
     board = Board.query.get(board_id)
     return board.to_json()
-    # return {
-    #         "title": board.title,
-    #         "owner": board.owner,
-    #         "cards": [card.to_json() for card in board.cards]
-    #         }
-# board routes
 
 # create board
 @board_bp.route("", methods=["POST"])
@@ -41,3 +36,5 @@ def get_boards():
     boards = Board.query.all()
     boards_response = [board.to_json() for board in boards]
     return jsonify(boards_response)
+
+# delete single board
--- app/card_routes.py
+++ app/card_routes.py
@@ -5,13 +5,7 @@ from app.models.card import Card
 # example_bp = Blueprint('example_bp', __name__)
 card_bp = Blueprint('card_bp', __name__, url_prefix="/boards/")
 
-#get all cards
-@card_bp.route("/cards", methods=["GET"], strict_slashes=False)
-def get_cards():
-    cards = Card.query.all()
-    cards_response = [card.to_json() for card in cards]
-    return jsonify(cards_response)
-
+# create new card to board by id
 @card_bp.route("<board_id>/cards", methods=["POST"], strict_slashes=False)
 def create_card(board_id):
     #validate
@@ -22,19 +16,6 @@ def create_card(board_id):
     db.session.commit()
 
     return make_response("Success", 201)
-
-@card_bp.route("<board_id>/cards/<card_id>", methods=["PATCH"], strict_slashes=False)
-def update_card(board_id, card_id):
-    #validate board_id
-    #validate card_id -- validate that card id is a valid ID
-    # and that it is assigned to the board
-    card = Card.query.get(card_id)
-
-    request_body = request.get_json()
-    card.message = request_body["message"]
-
-    db.session.commit()
-    return make_response("Success", 200)
 
 # delete single card
 @card_bp.route("<board_id>/cards/<card_id>", methods=["DELETE"], strict_slashes=False)
@@ -47,3 +28,26 @@ def delete_card(board_id, card_id):
     db.session.delete(card)
     db.session.commit()
     return make_response("Card successfully deleted", 200)
+
+
+# OPTIONAL ENHANCEMENT
+# update one card 
+# @card_bp.route("<board_id>/cards/<card_id>", methods=["PATCH"], strict_slashes=False)
+# def update_card(board_id, card_id):
+#     #validate board_id
+#     #validate card_id -- validate that card id is a valid ID
+#     # and that it is assigned to the board
+#     card = Card.query.get(card_id)
+
+#     request_body = request.get_json()
+#     card.message = request_body["message"]
+
+#     db.session.commit()
+#     return make_response("Success", 200)
+
+# #get all cards for all boards
+# @card_bp.route("/cards", methods=["GET"], strict_slashes=False)
+# def get_cards():
+#     cards = Card.query.all()
+#     cards_response = [card.to_json() for card in cards]
+#     return jsonify(cards_response)
