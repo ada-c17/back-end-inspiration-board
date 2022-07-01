ba042cb9893dbee19f67de8c530e5799a8a16924
Shayla Logan <shayla.logan709@gmail.com>
2022-07-01 09:47:29 -0700

    Changed to limit characters to 40

--- app/models/card.py
+++ app/models/card.py
@@ -2,7 +2,7 @@ from app import db
 
 class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    message = db.Column(db.String, nullable=False)
+    message = db.Column(db.String(40), nullable=False)
     likes_count = db.Column(db.Integer, autoincrement=True)
     board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'))
     board = db.relationship('Board',back_populates='cards')
