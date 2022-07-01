ed7abc2777063b37352f39d3b2f55b8130dac7b5
HaileyMatz <hmatz525@gmail.com>
2022-06-30 17:35:06 -0700

    chaged card.id to card.card_id

--- app/models/board.py
+++ app/models/board.py
@@ -4,7 +4,7 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
-    cards = db.relationship("Card", back_populates="board", lazy=True, cascade="all, delete", order_by="desc(Card.id)")
+    cards = db.relationship("Card", back_populates="board", lazy=True, cascade="all, delete", order_by="desc(Card.card_id)")
 
     def to_dict(self):
         return {
