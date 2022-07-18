d0dbdcdaf82be32229364c46dd3bb5d772e41cab
Gabriela Ibarra <gabriela.estefania.ibarra@gmail.com>
2022-07-18 17:44:06 -0500

    Tests for getting one board

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -17,9 +17,23 @@ def test_get_all_boards(client, create_three_boards):
     assert {"id": 2, "title": "Another Amazing Board", "owner": "User 2"} in response_body
     assert {"id": 3, "title": "The Best Board", "owner": "User 3"} in response_body
 
+def test_get_one_board(client, create_three_boards):
+    response = client.get("/boards/2")
+    response_body = response.get_json()
 
-# Test for Get one board
-    # Test that you get 404 error if board does not exist
+    assert response.status_code == 200
+    assert response_body == {"id": 2, 
+                            "title": "Another Amazing Board",
+                            "owner": "User 2", 
+                            "cards": []}
+
+def test_get_board_that_doesnt_exist(client, create_three_boards):
+    response = client.get("/boards/100")
+    response_body = response.get_json()
+
+    assert response.status_code == 404
+    assert response_body == {"message": "board 100 not found"}
+
 # Test for deleting one board
     # Test that you get 404 error if board does not exist
 # Test for Making a new board (with existing entries, and w/o?)
