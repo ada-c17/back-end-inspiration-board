ea32ccb11de93790c4ff8af420c9d509ef8553d4
diakymenko <80910811+diakymenko@users.noreply.github.com>
2022-07-19 14:36:19 -0700

    added limit to board number of characters

--- app/board_routes.py
+++ app/board_routes.py
@@ -5,6 +5,7 @@ from app.models.board import Board
 
 board_bp = Blueprint('boards', __name__, url_prefix="/boards")
 
+
 @board_bp.route("", methods=["POST"])
 def create_board():
     request_body = request.get_json()
@@ -19,14 +20,21 @@ def create_board():
             {
                 "details": "Please provide title!"
             }), 400
+    elif len(str((request_body["title"]))) or len(str((request_body["owner"]))) > 40:
+        return jsonify(
+            {
+                "details": "Please enter a message that is less than 40 characters!"
+            }), 400
 
     else:
-        new_board = Board(title=request_body["title"], owner=request_body["owner"])
+        new_board = Board(
+            title=request_body["title"], owner=request_body["owner"])
 
     db.session.add(new_board)
     db.session.commit()
 
     return make_response(jsonify(f"Board {new_board.title} with id {new_board.board_id} succesfully created"), 201)
+
 
 @board_bp.route("", methods=["GET"])
 def read_all_boards():
@@ -44,18 +52,21 @@ def read_all_boards():
         )
     return jsonify(boards_response)
 
+
 def validate_board(board_id):
     try:
         board_id = int(board_id)
     except:
-        abort(make_response({"message":f"board id {board_id} invalid"}, 400))
-    
+        abort(make_response({"message": f"board id {board_id} invalid"}, 400))
+
     board = Board.query.get(board_id)
 
     if not board:
-        abort(make_response({"message":f"board with {board_id} not found"}, 404))
-    
+        abort(make_response(
+            {"message": f"board with {board_id} not found"}, 404))
+
     return board
+
 
 @board_bp.route("/<board_id>", methods=["GET"])
 def read_one_board_with_all_cards(board_id):
@@ -64,10 +75,10 @@ def read_one_board_with_all_cards(board_id):
     cards = []
     for card in board.cards:
         cards.append({
-        "card_id":card.card_id,
-        "message": card.message,
-        "likes_count":card.likes_count,
-        "board_id": board_id
+            "card_id": card.card_id,
+            "message": card.message,
+            "likes_count": card.likes_count,
+            "board_id": board_id
         }
         )
     return {
