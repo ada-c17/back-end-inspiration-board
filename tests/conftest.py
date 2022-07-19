38a6b7c372921888e5b0aa34e6d3d1afb3d08039
Gabriela Ibarra <gabriela.estefania.ibarra@gmail.com>
2022-07-18 19:00:31 -0500

    Removed updated test configurations. Wrote tests for POST /boards route

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 import pytest
 from app import create_app
 from app import db
-from flask.signals import request_finished
 from app.models.board import Board
 from app.models.card import Card
 
@@ -9,10 +8,6 @@ from app.models.card import Card
 def app():
     # create the app with a test config dictionary
     app = create_app({"TESTING": True})
-
-    @request_finished.connect_via(app)
-    def expire_session(sender, response, **extra):
-        db.session.remove()
 
     with app.app_context():
         db.create_all()
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -53,9 +53,48 @@ def test_delete_board_that_doesnt_exist(client):
     assert response.status_code == 404
     assert response_body == {"message": "board 100 not found"}
 
-# Test for deleting one board
-    # Test that you get 404 error if board does not exist
-# Test for Making a new board (with existing entries, and w/o?)
-    # Test that 400 error is raised if invalid data is passed (so missing owner or title)
+# Test for Making a new board 
+def test_create_board(client):
+    response = client.post("/boards", json = {"owner": "New User", "title": "My New Inspiration Board"})
+    response_body = response.get_json()
+    
+    boards = Board.query.all()
+    new_board = Board.query.get(1)
+
+    assert response.status_code == 201
+    assert response_body == {"board":{
+            "id": 1,
+            "title": "My New Inspiration Board",
+            "owner": "New User",
+        }}
+    assert len(boards) == 1
+    assert new_board.title == "My New Inspiration Board"
+    assert new_board.owner == "New User"
+
+# Test that 400 error is raised if invalid data is passed (so missing owner or title)
+
+def test_create_board_must_contain_owner(client):
+    response = client.post("/boards", json = {"title": "My New Inspiration Board"})
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == {"details":"Invalid data"}
+    assert Board.query.all() == []
+
+def test_create_board_must_contain_title(client):
+    response = client.post("/boards", json = {"owner": "New User"})
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == {"details":"Invalid data"}
+    assert Board.query.all() == []
 
 # Maybe test for creating a new card associated w/ a board
+def test_create_card_on_board(client):
+    pass
+
+def test_create_card_on_board_must_contain_message(client):
+    pass
+
+def test_create_card_on_board_already_has_other_card(client, create_three_boards, add_card_to_board):
+    pass
