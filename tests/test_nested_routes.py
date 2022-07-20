cbdefac3d3fe8e25f4c417d8e8b41aa855d06875
Saria Pastor <saria.pastor@protonmail.com>
2022-07-20 14:51:26 -0400

    adds initial nested route tests

--- tests/test_nested_routes.py
+++ tests/test_nested_routes.py
@@ -7,3 +7,30 @@ def test_get_cards_by_board_id(client, one_board_w_three_cards):
     assert response.status_code == 200
     assert 'cards' in response_body
     assert len(response_body['cards']) == 3
+
+def test_delete_cards_by_board_id(client, one_board_w_three_cards):
+    response = client.delete('/boards/1/cards')
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert 'message' in response_body
+
+    board_response = client.get('/boards/1')
+    updated_board = board_response.get_json()['board']
+
+    assert board_response.status_code == 200
+    assert updated_board['cards'] == []
+
+def test_create_card_by_board_id(client, one_board):
+    card_params = {'message': 'The way Hope builds his House'}
+    response = client.post('/boards/1/cards', json=card_params)
+    response_body = response.get_json()
+
+    assert response.status_code == 201
+    assert 'card' in response_body
+    assert response_body['card'] == {
+        "card_id": 1,
+        "message": 'The way Hope builds his House',
+        "likes_count": 0,
+        "board_id": 1
+    }
