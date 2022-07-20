b275da3cebd6f21119be88007b9cf6adfb7a2bfa
Saria Pastor <saria.pastor@protonmail.com>
2022-07-20 15:33:55 -0400

    completes tests for nested routes

--- tests/test_nested_routes.py
+++ tests/test_nested_routes.py
@@ -34,3 +34,12 @@ def test_create_card_by_board_id(client, one_board):
         "likes_count": 0,
         "board_id": 1
     }
+
+def test_create_card_by_board_id_fails_w_empty_message(client, one_board):
+    card_params = {'message': ''}
+    response = client.post('/boards/1/cards', json=card_params)
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert 'message' in response_body
+    assert response_body['message'] == "Invalid data: New card needs a message"
