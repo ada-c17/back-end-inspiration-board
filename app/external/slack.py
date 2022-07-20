5ebf7c7aed76aae191c3cc05f141cf210960e6e4
sana <sanakakadan22@yahoo.com>
2022-07-20 22:25:15 +0200

    add slack notification for only the create card route

---
+++ app/external/slack.py
@@ -0,0 +1,16 @@
+import os
+import requests
+
+TOKEN = os.environ.get("SLACK_BOT_TOKEN")
+
+def notify_card_created(card_message):
+    data = {
+        "channel": "test-channel",
+        "text": f"Someone just created a card {card_message}"
+    }
+
+    headers = {
+        "Authorization": f"Bearer {TOKEN}"
+    }
+
+    requests.post('https://slack.com/api/chat.postMessage', data = data, headers = headers)
