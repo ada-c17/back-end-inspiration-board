2fd5da8519426a51624cb4263301bdf662fe1115
angelica <djyl.ahn@gmail.com>
2022-07-20 18:16:38 -0700

    ok another small fix

--- app/card_routes.py
+++ app/card_routes.py
@@ -10,7 +10,7 @@ card_bp = Blueprint('card_bp', __name__, url_prefix="/boards/")
 # create new card to board by id
 @card_bp.route("<board_id>/cards", methods=["POST"], strict_slashes=False)
 def create_card(board_id):
-    get_record_by_id(Board,board_id)
+    board = get_record_by_id(Board,board_id)
     
     request_body = request.get_json()
     new_card = make_record_safely(Card, request_body )
