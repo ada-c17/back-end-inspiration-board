a8e366f79d1ce1009426d46369eaa3f40ae08a21
angelica <djyl.ahn@gmail.com>
2022-07-20 18:47:00 -0700

    changes card model likes_count to not have default

--- app/models/card.py
+++ app/models/card.py
@@ -5,7 +5,7 @@ from flask import jsonify
 class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String, nullable=False)
-    likes_count = db.Column(db.Integer, default=0)
+    likes_count = db.Column(db.Integer, nullabe=False)
     board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'))
     board = db.relationship("Board", back_populates="cards")
 
