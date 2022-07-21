14c7588fd6a58dad765177fd91190f637d5e817b
Bahareh <izad@bu.edu>
2022-07-21 19:42:41 -0400

    testing lazy

--- app/models/board.py
+++ app/models/board.py
@@ -4,7 +4,7 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, nullable = False)
     owner = db.Column(db.String, nullable = False)
-    cards = db.relationship("Card", back_populates="board", lazy=True)
+    cards = db.relationship("Card", back_populates="board")
     
 
 
